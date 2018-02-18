0b432a9945a5401c88e7f419519f0623572d6d0a
EkiUbuntu <qinggojohn@gmail.com>
2018-02-18 10:14:05 +0800

    add comment

--- 1/molecule.h
+++ 1/molecule.h
@@ -11,20 +11,36 @@ class Molecule
 	double **geom;
 	string point_group;
 
+	//molecule_print.cc
 	void print_geom();
+
 	void rotate(double phi);
+
+	//molecule_tran
 	void translate(double x, double y, double z);
+
+	//molecule_length.cc
 	double bond(int atom1, int atom2);
+
+	//molecule_angle.cc
+	double unit(int cart, int atom1, int atom2); //得到atom1到atom2的单位向量的一个坐标
 	double angle(int atom1, int atom2, int atom3);
+
 	double torsion(int atom1, int atom2, int atom3, int atom4);
-	double oop(int atom1, int atom2, int atom3, int atom4);
-	double dihedral(int atom1, int atom2, int atom3, int atom4);
-	double unit(int cart, int atom1, int atom2);
-	double cross(int cart, int atom1, int atom2, int atom3, int atom4);
+
+	//molecule_oop.cc
+	double oop(int atom1, int atom2, int atom3, int atom4); //Out-of-Plane Angles
+
+  //molecule_dihedral.cc
+	double cross(int cart, int atom1, int atom2, int atom3, int atom4); //返回1-2与3-4叉乘函数的一个坐标
+	double dihedral(int atom1, int atom2, int atom3, int atom4); //1 - 2,3 - 4二面角
+
 	double ZvalToMass(int zvals);
+
+	//molecule_CenterofMass.cc
 	double CenterOfMass(int cart);
 
+	//molecule.cc
 	Molecule(const char *filename, int q);
 	~Molecule();
 };
-
--- 1/molecule_oop.cc
+++ 1/molecule_oop.cc
@@ -6,26 +6,24 @@
 #include <cmath>
 #include "molecule.h"
 
+//Out-of-Plane Angles
 double Molecule::oop(int atom1, int atom2, int atom3, int atom4)
 {
 	double oop=0, sinoop=0;
 	double vect1[3], vect2[3], vect3[3], vect4[3];
-	for(int i=0; i<3; i++)
-	{
-		//vect1[i] = unit(i,atom3,atom2);
-		//vect2[i] = unit(i,atom3,atom4);
-		//vect4[i] = unit(i,atom3,atom1);
-	}
+
 	for(int i=0; i<3; i++)
 	{
 		//vect3[i] = vect1[(i+1)%3]*vect2[(i+2)%3]-vect1[(i+2)%3]*vect2[(i+1)%3];
 		vect3[i] = cross(i, atom3, atom2, atom3, atom4);
 	}
+
 	for(int i=0; i<3; i++)
 	{
 		vect4[i] = unit(i,atom3,atom1);
 		sinoop += vect3[i]*vect4[i]/sin(angle(atom2, atom3, atom4));
 	}
+
 	//防止浮点数后面的位数超过函数定义域
 	if(sinoop < -1.0) oop = asin(-1.0);
 	else if(sinoop > 1.0) oop = asin(1.0);
